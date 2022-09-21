42f38b0ca0b94daae853bed7d449bc204e0c1c55
Jennifer Cotte <jennifer.malacain@laposte.net>
2022-09-21 11:31:47 +0200

    transformation des <aside> en <div>

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ for (let f of fleches) {
         if (fleche.classList.contains("fleche_gauche")) {
             if (liste_index != 0) {
                 films.style.setProperty("--liste-index", (liste_index - 1));
-                fleche.nextElementSibling.nextElementSibling.style.visibility =
-                     "visible";
+                fleche.parentElement.querySelector(".fleche_droite").style
+                    .visibility = "visible";
             };
             if (liste_index == 1) {
                 fleche.style.visibility = "hidden";
@@ -20,12 +20,12 @@ for (let f of fleches) {
         };
         if (fleche.classList.contains("fleche_droite")) {
             let nb_films = films.children.length;
-            let films_visibles = parseInt(getComputedStyle(films).getPropertyValue
-                ("--films-visibles"));
+            let films_visibles = parseInt(getComputedStyle(films).
+                getPropertyValue("--films-visibles"));
             let nb_pages = Math.ceil(nb_films / films_visibles);
             if ((liste_index + 1) < nb_pages) {
                 films.style.setProperty("--liste-index", (liste_index + 1));
-                fleche.previousElementSibling.previousElementSibling.style
+                fleche.parentElement.querySelector(".fleche_gauche").style
                     .visibility = "visible";
             };
             if ((liste_index + 2) == nb_pages) {
