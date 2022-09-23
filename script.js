dae29f81875b0a88ca2a4821a1d4a5a6f7b0e50d
Jennifer Cotte <jennifer.malacain@laposte.net>
2022-09-23 14:40:55 +0200

    ajout des films toutes categories

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// fonctionnement des flèches de déplacement des films
 let fleches = document.querySelectorAll(".fleches");
 
 for (let f of fleches) {
@@ -34,8 +35,83 @@ for (let f of fleches) {
         };
     });
 };
+// fin de : fonctionnement des flèches de déplacement des films
 
-let modale = document.getElementById("modale");
+// requêtes GET
+async function requete(url) {
+    let promise = await fetch(url)
+        .then (function(res) {
+            if (res.ok) {
+                return res.json();
+            };
+        })
+        .catch (function(err) {
+            console.log("Une erreur ressort de la requête");
+        });
+    return Promise.resolve(promise);
+};
+
+const base_url = "http://localhost:8000/api/v1/titles/"
+
+function image_film(num_film, html_film){
+    requete(base_url + num_film)
+    .then (function(value) {
+        html_film.style.backgroundImage = 'url("' + value.image_url + '")';
+        html_film.querySelector("span").innerText = value.id;
+    })
+};
+
+const base_url_1 = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
+const base_url_2 = "http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_"
+     + "score";
+
+let meilleur_film = document.getElementById("meilleur_film");
+let mieux_notes = document.querySelectorAll("#films_mieux_notes .films a");
+
+requete(base_url_1) // requete des meilleurs films (page 1)
+.then(function(value){
+    let num_film = value.results[0].id;
+    requete(base_url + num_film) // requete DU meilleur film
+    .then(function(value){
+        meilleur_film.querySelector("h1").innerText = value.title;
+        meilleur_film.querySelector("p").innerText = value.description;
+        meilleur_film.style.backgroundImage = 'url("' + value.image_url + '")';
+        meilleur_film.querySelector("span").innerText = value.id;
+    });
+    for (let i = 0; i < 4; i++) {
+        image_film(value.results[i+1].id, mieux_notes[i])
+    };
+});
+
+requete(base_url_2) // requete des meilleurs films (page 2)
+.then(function(value){
+    for (let i = 0; i < 3; i++) {
+        image_film(value.results[i].id, mieux_notes[i+4]);
+    };
+});
+
+const genres = ["action", "comedy", "sci-fi"];
+genres.forEach(function(genre) {
+    let categorie = document.getElementById(genre);
+    let films = categorie.querySelectorAll(".films a");
+    requete(base_url_1 + "&genre=" + genre) // requete des films par genre (p1)
+    .then(function(value){
+        for (let i = 0; i < 6; i++) {
+            image_film(value.results[i].id, films[i]);
+        };
+    });
+    requete(base_url_2 + "&genre=" + genre) // requete des films par genre (p2)
+    .then(function(value){
+        for (let i = 0; i < 3; i++) {
+            image_film(value.results[i].id, films[i+5]);
+        };
+    });
+});
+// fin de : requêtes GET
+
+// gestion de la page modale
+const modale = document.getElementById("modale");
+
 function infos_modale(value) {
     modale.querySelector("h1").innerText = value.title;
     modale.querySelector("#genre").innerText = "Genre : " + value.genres;
@@ -60,77 +136,15 @@ function infos_modale(value) {
     modale.querySelector("img").src = value.image_url;
 };
 
-function infos_film(num_film, html_film) {
-// recherche des informations spécifiques à ce film
-    fetch("http://localhost:8000/api/v1/titles/" + num_film)
-    .then (function(res) {
-        if (res.ok) {
-            return res.json();
-        };
-    })
-    .then (function(value) {
-        html_film.style.backgroundImage = 'url("' + value.image_url + '")';
-        infos_modale(value);
-    })
-    .catch (function(err) {
-        console.log("Une erreur ressort de la requête du film")
+let infos = document.querySelectorAll(".infos");
+
+for (let a of infos) {
+    a.addEventListener("click", function(e) {
+        let num_film = a.querySelector("span").textContent
+        requete(base_url + num_film)
+        .then(function(value){
+            infos_modale(value);
+        });
     });
 };
-
-let meilleur_film = document.getElementById("meilleur_film");
-let mieux_notes = document.querySelectorAll("#films_mieux_notes .films a")
-
-fetch ("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
-// recherche du meilleur film en les filtrant par leur score imdb
-    .then (function(res) {
-        if (res.ok) {
-            return res.json();
-        };
-    })
-    .then (function(value) {
-        let num_film = value.results[0].id;
-        // isole l'id du meilleur film
-        fetch("http://localhost:8000/api/v1/titles/" + num_film)
-            .then (function(res) {
-                if (res.ok) {
-                    return res.json();
-                };
-            })
-            .then (function(value) {
-                meilleur_film.querySelector("h1").innerText = value.title;
-                meilleur_film.querySelector("p").innerText = value.description;
-                meilleur_film.style.backgroundImage = 'url("' + value.
-                    image_url + '")';
-                infos_modale(value);
-            })
-            .catch (function(err) {
-                console.log("Une erreur ressort de la requête du meilleur " +
-                "film")
-            });
-        for (let i = 0; i < 4; i++) {
-            infos_film(value.results[i+1].id, mieux_notes[i])
-        };
-    })
-    .catch (function(err) {
-        console.log("Une erreur ressort de la requête des films filtrés par "
-            + "score imdb (page 1)")
-    });
-
-fetch ("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score")
-    .then (function(res) {
-        if (res.ok) {
-            return res.json();
-        };
-    })
-    .then (function(value) {
-        for (let i = 0; i < 3; i++) {
-            infos_film(value.results[i].id, mieux_notes[i+4]);
-        };
-    })
-    .catch (function(err) {
-        console.log("Une erreur ressort de la requête des films filtrés par "
-            + "score imdb (page 2)")
-    });
-
-let genres = [action, comedy, sci-fy];
-
+// fin de : gestion de la page modale
